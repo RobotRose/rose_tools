5ff0a6773afbdaf56f985aea0eef5d56900545d8
ohendriks <hendriks@robot-rose.nl>
2015-03-18 18:17:24 +0100

    Updated calibrate_kinect.sh

--- scripts/calibrate_kinect.sh
+++ scripts/calibrate_kinect.sh
@@ -13,10 +13,27 @@ pushd .
 echo "Starting Kinect calibration." | colorize BLUE
 echo "Connect the Kinect to calibrate to your local PC via USB. Then, run $ roslaunch openni_launch openni.launch #in a separate terminal and press <enter> here when done" | colorize BLUE
 read -n1
-echo "Starting calibration. When the calibration window appears, move the checkboard around, through every angle, distance and position visible to the kinect" | colorize YELLOW
+echo "Starting calibration. When the calibration window appears, move the checkerboard around, through every angle, distance and position visible to the kinect" | colorize YELLOW
 echo "Once the bars in the right of the window are all green, click the 'Calibrate' button. This starts a lengthy calculation" | colorize YELLOW
 echo "When satisfied with the result, click 'Save'." | colorize YELLOW #When Save is clicked, the terminal prints the save location, e.g. /tmp/calibrationdata.tar.gz
 echo "Finally, click 'Commit'" | colorize YELLOW
+echo ""
+
+# Check the KINECT_SERIAL storage location
+CALIBRATION_STORAGE_RGB=`rospack find rose_config`/kinect/rgb_${KINECT_SERIAL}.yaml
+CALIBRATION_STORAGE_DEPTH=`rospack find rose_config`/kinect/depth_${KINECT_SERIAL}.yaml
+
+if [ -f $CALIBRATION_STORAGE_RGB ]; then
+    echo "Warning a calibration file already exists at ${CALIBRATION_STORAGE_RGB}" | colorize RED
+else
+    echo "RGB calibration will be saved to ${CALIBRATION_STORAGE_RGB}." | colorize GREEN
+fi
+
+if [ -f $CALIBRATION_STORAGE_DEPTH ]; then
+    echo "Warning a calibration file already exists at ${CALIBRATION_STORAGE_DEPTH}" | colorize RED
+else
+    echo "Depth calibration will be saved to ${CALIBRATION_STORAGE_DEPTH}." | colorize GREEN
+fi
 
 rosrun camera_calibration cameracalibrator.py image:=/camera/rgb/image_raw camera:=/camera/rgb --size ${CROSSINGS} --square ${SQUARESIZE}
 
@@ -27,6 +44,9 @@ else
     exit 1
 fi
 
+# Check if the tool did indeed generate its calibration file
+SAVEFILE1=/tmp/calibrationdata.tar.gz
+
 if [ -f $SAVEFILE1 ]; then
     echo "Calibration window saved its data to ${SAVEFILE1}" | colorize GREEN
 else
@@ -34,8 +54,7 @@ else
     exit 1
 fi
 
-SAVEFILE1=/tmp/calibrationdata.tar.gz
-echo "Unpacking calibration file..." | colorize BLUE
+echo "Unpacking RGB calibration file..." | colorize BLUE
 mkdir -p ~/.ros/calibation_rgb
 cd ~/.ros/calibation_rgb
 tar zxfv ${SAVEFILE1}
@@ -44,7 +63,7 @@ echo "Renaming file to .ini" | colorize BLUE
 mv ost.txt ost.ini #Rename the file
 
 echo "Converting .ini to Kinect yaml calibration file" | colorize BLUE
-rosrun camera_calibration_parsers convert ost.ini `rospack find rose_config`/kinect/rgb_${KINECT_SERIAL}.yaml
+rosrun camera_calibration_parsers convert ost.ini ${CALIBRATION_STORAGE_RGB}
 if [ $? -eq 0 ]; then
     echo "Conversion successful" | colorize GREEN
 else
@@ -56,11 +75,13 @@ fi
 
 echo "Now block off the IR projector and relaunch $ roslaunch openni_launch openni.launch #in a separate terminal and press <enter> here when done" | colorize YELLOW
 read -n1
-echo "Starting calibration. When the calibration window appears, move the checkboard around, through every angle, distance and position visible to the kinect" | colorize YELLOW
+echo "Starting calibration. When the calibration window appears, move the checkerboard around, through every angle, distance and position visible to the kinect" | colorize YELLOW
 echo "Once the bars in the right of the window are all green, click the 'Calibrate' button. This starts a lengthy calculation" | colorize YELLOW
 echo "When satisfied with the result, click 'Save'." | colorize YELLOW #When Save is clicked, the terminal prints the save location, e.g. /tmp/calibrationdata.tar.gz
 echo "Finally, click 'Commit'" | colorize YELLOW
+
 rosrun camera_calibration cameracalibrator.py image:=/camera/ir/image_raw camera:=/camera/ir --size ${CROSSINGS} --square ${SQUARESIZE}
+
 if [ $? -eq 0 ]; then
     echo "Calibration ran successfuly" | colorize GREEN
 else
@@ -71,22 +92,22 @@ fi
 SAVEFILE2=/tmp/calibrationdata.tar.gz
 
 if [ -f $SAVEFILE2 ]; then
-    echo "Calibration window saved to ${SAVEFILE}" | colorize GREEN
+    echo "Calibration window saved to ${SAVEFILE2}" | colorize GREEN
 else
-    echo "Calibration should be saved to ${SAVEFILE} but file does not exist" | colorize RED
+    echo "Calibration should be saved to ${SAVEFILE2} but file does not exist" | colorize RED
     exit 1
 fi
 
-echo "Unpacking calibration file..." | colorize BLUE
-mkdir -p ~/.ros/calibation_ir
-cd ~/.ros/calibation_ir
+echo "Unpacking depth calibration file..." | colorize BLUE
+mkdir -p ~/.ros/calibation_depth
+cd ~/.ros/calibation_depth
 tar zxfv ${SAVEFILE2}
 
 echo "Renaming file to .ini" | colorize BLUE
 mv ost.txt ost.ini #Rename the file
 
 echo "Converting .ini to Kinect yaml calibration file" | colorize BLUE
-rosrun camera_calibration_parsers convert ost.ini `rospack find rose_config`/kinect/depth_${KINECT_SERIAL}.yaml
+rosrun camera_calibration_parsers convert ost.ini ${CALIBRATION_STORAGE_DEPTH}
 if [ $? -eq 0 ]; then
     echo "Conversion successful" | colorize GREEN
 else
@@ -95,9 +116,9 @@ else
 fi
 
 roscd rose_config
-git add `rospack find rose_config`/kinect/rgb_${KINECT_SERIAL}.yaml `rospack find rose_config`/kinect/depth_${KINECT_SERIAL}.yaml
-git commit `rospack find rose_config`/kinect/rgb_${KINECT_SERIAL}.yaml `rospack find rose_config`/kinect/depth_${KINECT_SERIAL}.yaml -m "Added Kinect calibration data for ${KINECT_SERIAL}"
-echo "Committed calibration to GIT" | colorize GREEN
+git add ${CALIBRATION_STORAGE_RGB} ${CALIBRATION_STORAGE_DEPTH}
+git commit ${CALIBRATION_STORAGE_RGB} ${CALIBRATION_STORAGE_DEPTH} -m "Added/updated Kinect calibration data for ${KINECT_SERIAL}"
+echo "Committed calibration to GIT." | colorize GREEN
 
 popd
 
