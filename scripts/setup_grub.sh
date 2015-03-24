3c113f85bdbf7d87c56fb62e78d6a801623418c5
ohendriks <hendriks@robot-rose.nl>
2015-03-24 12:48:58 +0100

    Typo's

--- scripts/setup_grub.sh
+++ scripts/setup_grub.sh
@@ -3,33 +3,33 @@
 DEFAULT_GRUB_CONFIG_FILE="$ROSE_TOOLS/scripts/default_grub_config"
 GRUB_CONFIG_FILE="/etc/default/grub"
 
-echo "Making backup of grub config file."
+echo "Making backup of grub configuration file."
 
 cp $GRUB_CONFIG_FILE ${GRUB_CONFIG_FILE}.bak
 
 if [ $? -ne 0 ]; then
-    echo "Error while making backup of config file, aborting grub configuration."
+    echo "Error while making backup of configuration file, aborting grub configuration."
     exit 1
 fi
 
-echo "Copying grub config file."
+echo "Copying grub configuration file."
 
 cp $DEFAULT_GRUB_CONFIG_FILE $GRUB_CONFIG_FILE
 
 if [ $? -ne 0 ]; then
-    echo "Error while copying new config file, aborting grub configuration."
+    echo "Error while copying new configuration file, aborting grub configuration."
     exit 1
 fi
 
-echo "Applying grub config file."
+echo "Applying grub configuration file."
 
 update-grub
 
 if [ $? -ne 0 ]; then
-    echo "Error while apllying new config file, configuration failed."
+    echo "Error while applying new configuration file, configuration failed."
     exit 1
 else
-    echo "Grub config succesfully installed."
+    echo "Grub configuration successfully installed."
 fi
 
 exit 0
