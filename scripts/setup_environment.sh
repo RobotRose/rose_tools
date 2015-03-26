e595a4bbb3de83ee4c77dab27552de741520b6d8
ohendriks <hendriks@robot-rose.nl>
2015-03-26 18:20:00 +0100

    Make setup_environment use ops instead of simple args.

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,27 +1,38 @@
 #!/bin/bash  
 
-if [ "$1" == "" ]; then
-	DEPLOYMENT_SCRIPT="/usr/bin/deployment_script.sh"
-else
-	DEPLOYMENT_SCRIPT="$1"
-fi
+DEPLOYMENT_SCRIPT="/usr/bin/deployment_script.sh"
+DEPLOYMENT_FILE="/usr/bin/deployment_file.sh"
 
-if [ "$2" == "" ]; then
-	DEPLOYMENT_FILE="/usr/bin/deployment_file.sh"
-else
-	DEPLOYMENT_FILE="$2"
-fi
+while getopts ":s:d:" opt; do
+  case $opt in
+    s)
+      echo "Using $OPTARG ad deployment script." >&2
+      DEPLOYMENT_SCRIPT=$OPTARG
+      ;;
+    d)
+      echo "Using $OPTARG ad deployment file." >&2
+      DEPLOYMENT_FILE=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 # Activate Deployment
 if [ -e ${DEPLOYMENT_SCRIPT} ]; then
 	if [ -e ${DEPLOYMENT_FILE} ]; then	
-		echo "Sourcing deployment file '$(readlink -f ${DEPLOYMENT_FILE})'." | colorize GREEN
 		source ${DEPLOYMENT_SCRIPT} ${DEPLOYMENT_FILE}
 	else
-		>&2 echo "Deployment file ${DEPLOYMENT_FILE} links to '$(readlink -f ${DEPLOYMENT_FILE})' not found." | colorize RED
+		>&2 echo "Deployment file ${DEPLOYMENT_FILE} | $(readlink -f ${DEPLOYMENT_FILE}) not found." | colorize RED
 	fi
 else
-	>&2 echo "Deployment script ${DEPLOYMENT_SCRIPT} links to '$(readlink -f ${DEPLOYMENT_SCRIPT})' not found." | colorize RED
+	>&2 echo "Deployment script ${DEPLOYMENT_SCRIPT} | $(readlink -f ${DEPLOYMENT_SCRIPT}) not found." | colorize RED
 fi
 
 echo -n 'WORKSPACES FILE = ' | colorize YELLOW
