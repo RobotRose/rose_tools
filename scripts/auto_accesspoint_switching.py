11f56ab55678d4fd20d3e25893a141ca2d71a0ef
ohendriks <hendriks@robot-rose.nl>
2015-04-02 00:59:17 +0200

    Refreshed auto_accesspoint_switching.py

--- scripts/auto_accesspoint_switching.py
+++ scripts/auto_accesspoint_switching.py
@@ -21,7 +21,7 @@ Options:
   --whitelistfile=<white_list_file>  Full path indicating the white list file. Default white listed AP ID is ROSE_WIFI.
   --low=<low_percentage>  As soon as signal strength is below this percentage [0-100%], of the average signal strength, switching will be preferred.
   --impr=<improvement_percentage>  The minimal improvement to gain from switching [0-100%].
-  --delay=<minimal_delay>  Minimal switching delay in seconds [default: 10].
+  --delay=<minimal_delay>  Minimal switching delay in seconds [default: 10.0].
 """
 
 from docopt import docopt
@@ -33,54 +33,73 @@ import sys
 import time
 
 
-def get_raw_current_ap():
+def get_current_ap_bssid():
     current_ap = {}
-    raw = iwconfig(arguments["--interface"])
-    current_ap["BSSID"] = [line.split("Access Point: ", 1)[1].strip().encode("ascii", "ignore") for line in raw.split("\n") if "Access Point: " in line][0]
-    current_ap["dBm"]   = [float(line.split("Signal level=", 1)[1].strip().split(" ", 1)[0].encode("ascii", "ignore")) \
-                            for line in raw.split("\n") if "Signal level=" in line]
-    print current_ap["dBm"]
-    return current_ap
+    try: raw = wpa_cli("status", arguments["--interface"])
+    except ErrorReturnCode:
+        print "Could not get wpa_cli status {0}".format(arguments["--interface"])
+        return None
 
-def get_raw_scan():
-    return egrep(iwlist(arguments["--interface"], "scan"), "-A 5", "-B 5", "ROSE_WIFI")
+    key = raw.split("\n")[1].split("=")[0]
+    if key == "bssid":
+        bssid = raw.split("\n")[1].split("=")[1]
+    else:
+        bssid = None
+    return bssid
 
-def get_raw_aps(rawscan):
+def get_current_ap(aps):
+    if len(aps) == 0:
+        return None
 
-    aps = rawscan.split('--')
+    current_bssid = get_current_ap_bssid()
+    if get_current_ap_bssid == None:
+        return None
+
+    aps_with_correct_bssid = [ap for ap in aps if ap["BSSID"] == current_bssid]
+    if len(aps_with_correct_bssid) > 1:
+        print "More than one AP found with BSSID {0}, taking first.".format(current_bssid)
+    
+    if len(aps_with_correct_bssid) == 0:
+        return None
+
+    return aps[0]
+    
+def force_scan():
+    try:  wpa_cli("scan", arguments["--interface"])
+    except ErrorReturnCode:
+        print "Could not run wpa_cli scan {0}".format(arguments["--interface"])
+
+def get_latest_raw_scan():
+    # Limit the rate
+    time.sleep(0.1)
+    
+    try: raw = wpa_cli("scan_results", arguments["--interface"])
+    except ErrorReturnCode:
+        print "Could not run wpa_cli scan_results {0}".format(arguments["--interface"])
+        return ""
+
+    return raw
+
+def get_aps(rawscan):
+    aps_list = []
+    aps = rawscan.split('\n')
     for ap in aps:
-        parts = ap.split("  ")
+        parts = ap.split("\t")
+        # bssid / frequency / signal level / flags / ssid
 
-        def parse_raw(parts):
-            for part in parts:
-                part = part.replace("=", ":").strip()
-                split = part.split(':', 1)
-                if len(split) >= 2:
-                    yield split[0], split[1].replace('"', '').strip()
-
-        values = dict(parse_raw(parts))
-        yield values
+        if "ROSE_WIFI" in parts:
+            values = {}
+            values["BSSID"]     = parts[0].encode("ascii", "ignore")
+            values["frequency"] = float(parts[1].encode("ascii", "ignore"))
+            values["dBm"]       = float(parts[2].encode("ascii", "ignore"))
+            values["flags"]     = parts[3].encode("ascii", "ignore")
+            values["ESSID"]     = parts[4].encode("ascii", "ignore")
+            aps_list += [values]
+    
+    return aps_list
 
 def switch_to_ap(access_point_bssid):
     wpa_cli("roam", access_point_bssid) 
-
-def process_raw_scan(rawscan):
-    mac_map = {}
-    raw_aps = list(get_raw_aps(rawscan))
-    for ap in raw_aps:
-        mac = [value.encode("ascii", "ignore") for key,value in ap.iteritems() if "Address" in key][0]
-        mac_map[mac] = ap
-
-    # pprint.pprint(mac_map)
-
-    # Pre-Process data items
-    for mac, data in mac_map.iteritems():
-        for k,v in data.iteritems():
-            if k == "Signal level":
-                parts = v.split(" ")
-                data[k] = float(parts[0])
-
-    return mac_map
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='WiFi switching')
@@ -98,57 +117,88 @@ if __name__ == '__main__':
     else:
         arguments["--impr"] = float(arguments["--impr"])
 
-    switch_to_ap("38:2C:4A:66:E9:40")
+    # switch_to_ap("38:2C:4A:66:E9:40")
+    switched_time = time.time()
+    scanned_time = time.time()
 
     while 1:
-        current_access_point = get_raw_current_ap()
-        # pprint.pprint("Current access point: {0} | {1}".format(current_access_point["BSSID"], current_access_point["dBm"]))
+        elapsed_time = time.time() - scanned_time
+        print "Elapsed time since last scan: {0}".format(elapsed_time)
+        if elapsed_time >= float(10.0):
+            print "Scanning..."
+            force_scan()
+            scanned_time = time.time()
 
-        mac_map = process_raw_scan(get_raw_scan())
+        aps = get_aps(get_latest_raw_scan())
+        current_access_point = get_current_ap(aps)
+        if current_access_point == None:
+            print "Could not fetch current access point, retrying in 10s..."
+            time.sleep(10)
+            continue
 
-        pprint.pprint(mac_map)
+        # pprint.pprint("Current access point: {0} | {1} dBm".format(current_access_point["BSSID"], current_access_point["dBm"]))
+        # pprint.pprint(aps)
 
-        # Check for len(mac_map is not null)
-        average = sum([data["Signal level"] for mac, data in mac_map.iteritems()])/len(mac_map)
+        # # Check for len(mac_map is not null)
+        average = 0
+        if len(aps) != 0:
+            for ap in aps:
+                average += ap["dBm"]
 
-        precentage_of_average = 100-(current_access_point["dBm"]/average) * 100.0
+            average /= len(aps)
 
-        macs = []
-        candidate_macs = []
+        ap_list = []
+        for ap in aps:
+            ap_list += [[ap["BSSID"], ap["frequency"], ap["dBm"], ap["flags"], ap["ESSID"]]]
 
-        os.system('cls' if os.name == 'nt' else 'clear')
+        sorted_ap_list = sorted(ap_list, key=lambda x: x[2], reverse=True)
 
-        for mac, data in mac_map.iteritems():
-            macs += [[mac, mac_map[mac]["Signal level"], mac_map[mac]["ESSID"]]]
-
-        sorted_macs = sorted(macs, key=lambda x: x[1], reverse=True)
-
+        # print sorted_ap_list
         current_bssid = current_access_point["BSSID"]
         current_signal_level = current_access_point["dBm"]
-        for mac, signal_level, essid in sorted_macs:
+        
+        
+        os.system('cls' if os.name == 'nt' else 'clear')
+        candidate_aps = []
+        for ap in sorted_ap_list:
+            bssid   = ap[0]
+            freq    = ap[1]
+            dbm     = ap[2]
+            flags   = ap[3]
+            essid   = ap[4]
 
-            percentage_diff_with_current = (current_signal_level/signal_level) * 100.0 - 100
+            precentage_of_average = 100-(dbm/average) * 100.0
+            percentage_diff_with_current = (current_signal_level/dbm) * 100.0 - 100
             
-
-            if mac == current_bssid:
-                cprint("* {0} | {1:3.0f} dBm | {2: 3.2f}% | {3}".format(mac, signal_level, precentage_of_average, essid), 'blue')
+            if bssid == current_bssid:
+                cprint("*\t{0} | {1:3.0f} dBm | {2: 3.2f}% | {3: 3.2f}% | {4}".format(bssid, dbm, precentage_of_average, percentage_diff_with_current, essid), 'blue')
             else:
                 if percentage_diff_with_current >= arguments["--impr"]:
-                    candidate_macs += [[mac, signal_level, essid]]
-                    cprint("  {0} | {1:3.0f} dBm | {2: 3.2f}% | {3}".format(mac, signal_level, percentage_diff_with_current, essid), 'green')
+                    candidate_aps += [[bssid, dbm, essid]]
+                    cprint("\t{0} | {1:3.0f} dBm | {2: 3.2f}% | {3: 3.2f}% | {4}".format(bssid, dbm, precentage_of_average, percentage_diff_with_current, essid), 'green')
                 else:
-                    cprint("  {0} | {1:3.0f} dBm | {2: 3.2f}% | {3}".format(mac, signal_level, percentage_diff_with_current, essid), 'red')
+                    cprint("\t{0} | {1:3.0f} dBm | {2: 3.2f}% | {3: 3.2f}% | {4}".format(bssid, dbm, precentage_of_average, percentage_diff_with_current, essid), 'red')
                 
         
-        print "  -------------------------------"
-        print "  Average             {0:3.0f} dBm".format(average)
+        print "\t-------------------------------"
+        print "\tAverage             {0:3.0f} dBm".format(average)
 
 
-        
+        elapsed_time = time.time() - switched_time
+        print "Elapsed time since last switch: {0}".format(elapsed_time)
+
         print "Sorted candidates:"
-        pprint.pprint(candidate_macs)
+        pprint.pprint(candidate_aps)
 
-        if len(candidate_macs) > 0:
-            print "Switching to access point {0}".format(candidate_macs[0])
-            switch_to_ap(candidate_macs[0][0])
-            
+
+        if len(candidate_aps) > 0 and elapsed_time >= float(arguments["--delay"]):
+             print "Switching to access point {0}".format(candidate_aps[0])
+             switch_to_ap(candidate_aps[0][0])
+             switch_time = time.time()
+
+        elif len(candidate_aps) > 0 and elapsed_time < float(arguments["--delay"]):
+             print "Not switching to access point {0}, because of minimal delay between switching: {1}/{2}".format(candidate_aps[0][0], elapsed_time, arguments["--delay"])
+
+
+
+             
